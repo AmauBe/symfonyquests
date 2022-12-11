d62eb86ed90e933273aa00aa3677ca23d54a14c0
AmauBE <amaury.beurrier@gmail.com>
2022-12-11 08:28:41 +0100

    quests 11

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -11,80 +11,105 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $program = new program(); 
-        $program->setTitle('The Walking Dead');
-        $program->setSynopsis('Des zombies envahissent le monde');
-        $program->setCategory($this->getReference('category_Horreur'));
-        $manager->persist($program);
-
-
         $program1 = new program(); 
-        $program1->setTitle('Warrior Nun');
-        $program1->setSynopsis("Histoire d'un groupe de religieuse guerrière");
-        $program1->setCategory($this->getReference('category_Action'));
+        $program1->setTitle('The Walking Dead');
+        $program1->setSynopsis('Des zombies envahissent le monde');
+        $program1->setCategory($this->getReference('category_Horreur'));
+        $program1->setYear('2010');
+        $program1->setCountry('Etats-Unis');
         $manager->persist($program1);
 
 
         $program2 = new program(); 
-        $program2->setTitle('Simpsons');
-        $program2->setSynopsis('Aventure de la famille simpsons');
-        $program2->setCategory($this->getReference('category_Animation'));
+        $program2->setTitle('Warrior Nun');
+        $program2->setSynopsis("Histoire d'un groupe de religieuse guerrière");
+        $program2->setCategory($this->getReference('category_Action'));
+        $program2->setYear('2020');
+        $program2->setCountry('Etats-Unis');
         $manager->persist($program2);
 
+
         $program3 = new program(); 
-        $program3->setTitle('The witcher');
-        $program3->setSynopsis('Série reprenant le célébre jeux vidéo');
-        $program3->setCategory($this->getReference('category_Fantastique'));
+        $program3->setTitle('Simpsons');
+        $program3->setSynopsis('Aventure de la famille simpsons');
+        $program3->setCategory($this->getReference('category_Animation'));
+        $program3->setYear('1989');
+        $program3->setCountry('Etats-Unis');
         $manager->persist($program3);
 
-
         $program4 = new program(); 
-        $program4->setTitle('Le seigneur des Anneaux');
-        $program4->setSynopsis("Nouvelle histoire basé sur l'oeuvre de Tolkien mais en série cette fois-ci");
-        $program4->setCategory($this->getReference('category_Aventure'));
+        $program4->setTitle('The witcher');
+        $program4->setSynopsis('Série reprenant le célébre jeux vidéo');
+        $program4->setCategory($this->getReference('category_Fantastique'));
+        $program4->setYear('2019');
+        $program4->setCountry('Etats-Unis');
         $manager->persist($program4);
 
+
         $program5 = new program(); 
-        $program5->setTitle('Friends');
-        $program5->setSynopsis("Aventure d'un groupe d'ami");
-        $program5->setCategory($this->getReference('category_Comedie'));
+        $program5->setTitle('Le seigneur des Anneaux');
+        $program5->setSynopsis("Nouvelle histoire basé sur l'oeuvre de Tolkien mais en série cette fois-ci");
+        $program5->setCategory($this->getReference('category_Aventure'));
+        $program5->setYear('2022');
+        $program5->setCountry('Etats-Unis');
         $manager->persist($program5);
 
+        $program6 = new program(); 
+        $program6->setTitle('Friends');
+        $program6->setSynopsis("Aventure d'un groupe d'ami");
+        $program6->setCategory($this->getReference('category_Comedie'));
+        $program6->setYear('1994');
+        $program6->setCountry('Etats-Unis');
+        $manager->persist($program6);
+
         $program7 = new program(); 
-        $program7->setTitle('Avenger');
-        $program7->setSynopsis('Aventure groupe heros');
+        $program7->setTitle('The Mandalorian');
+        $program7->setSynopsis("Aventure d'un personnage faisant partie de la communauté des Mandalorian, univers Star Wars ");
         $program7->setCategory($this->getReference('category_Sf'));
+        $program7->setYear('2019');
+        $program7->setCountry('Etats-Unis');
         $manager->persist($program7);
 
         $program8 = new program(); 
         $program8->setTitle('MacGyver');
         $program8->setSynopsis("Aventure d'un homme se sortant des situations les plus difficiles grâce à son génie");
         $program8->setCategory($this->getReference('category_Action'));
+        $program8->setYear('1985');
+        $program8->setCountry('Etats-Unis');
         $manager->persist($program8);
 
         $program9 = new program(); 
         $program9->setTitle('The 100');
         $program9->setSynopsis("Un groupe d'adolescent se retrouve sur une nouvelle terre");
         $program9->setCategory($this->getReference('category_Action'));
+        $program9->setYear('2010');
+        $program9->setCountry('Etats-Unis');
         $manager->persist($program9);
 
         $program10 = new program(); 
         $program10->setTitle('The terror');
         $program10->setSynopsis('Histoire de Sir John Franklin menant une expédition à la découverte du Passage du Nord-Ouest');
         $program10->setCategory($this->getReference('category_Horreur'));
+        $program10->setYear('2010');
+        $program10->setCountry('Etats-Unis');
         $manager->persist($program10);
 
         $program11 = new program(); 
-        $program11->setTitle('American Horror Story');
-        $program11->setSynopsis("Différentes histoire à travers les saisons");
+        $program11->setTitle('Supernatural');
+        $program11->setSynopsis("Histoire de deux frères qui chassent les monstres les plus horribles de la planétes");
         $program11->setCategory($this->getReference('category_Horreur'));
+        $program11->setYear('2015');
+        $program11->setCountry('Etats-Unis');
         $manager->persist($program11);
 
         $program12 = new program(); 
         $program12->setTitle('Fear the Walking Dead');
         $program12->setSynopsis('Basé sur the walking dead mais sur la cote ouest');
         $program12->setCategory($this->getReference('category_Horreur'));
+        $program12->setYear('2010');
+        $program12->setCountry('Etats-Unis');
         $manager->persist($program12);
+        $this->addReference('program_' . $key, $program);
 
         $manager->flush();
     }
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -3,8 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\ProgramRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: ProgramRepository::class)]
 class Program
@@ -23,9 +25,51 @@ class Program
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $poster = null;
 
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\ManyToOne(inversedBy: 'program')]
     private ?Category $category = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $country = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    #[ORM\OneToMany(mappedBy: 'program', targetEntity: Season::class)]
+    private Collection $seasons;
+
+    public function __construct()
+{
+    $this->programs = new ArrayCollection();
+    $this->seasons = new ArrayCollection();
+}
+
+public function getPrograms(): Collection
+{
+    return $this->programs;
+}
+
+public function addProgram(Program $program): self
+{
+    if (!$this->programs->contains($program)) {
+        $this->programs->add($program);
+        $program->setCategory($this);
+    }
+
+    return $this;
+}
+
+public function removeProgram(Program $program): self
+{
+    if ($this->programs->removeElement($program)) {
+        // set the owning side to null (unless already changed)
+        if ($program->getCategory() === $this) {
+            $program->setCategory(null);
+        }
+    }
+
+    return $this;
+}
+
 
     public function getId(): ?int
     {
@@ -76,6 +120,60 @@ class Program
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function setYear(string $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Season>
+     */
+    public function getSeasons(): Collection
+    {
+        return $this->seasons;
+    }
+
+    public function addSeason(Season $season): self
+    {
+        if (!$this->seasons->contains($season)) {
+            $this->seasons->add($season);
+            $season->setProgram($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSeason(Season $season): self
+    {
+        if ($this->seasons->removeElement($season)) {
+            // set the owning side to null (unless already changed)
+            if ($season->getProgram() === $this) {
+                $season->setProgram(null);
+            }
+        }
 
         return $this;
     }
